f15ef1e7bdf7bb22e220620e7bfb1f176084609a
Mike Rehberg <git@TR182.com>
2015-01-08 19:39:20 -0500

    Gyro not working

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -7,24 +7,25 @@ class Robot: public SampleRobot
 {
 
     // Channels for the wheels
-    const static int frontLeftChannel	= 0;
-    const static int rearLeftChannel	= 1;
-    const static int frontRightChannel	= 2;
-    const static int rearRightChannel	= 3;
-
-    const static int joystickChannel	= 0;
+    const static int frontLeftChannel	= 1;
+    const static int rearLeftChannel	= 0;
+    const static int frontRightChannel	= 3;
+    const static int rearRightChannel	= 2;
+    const static int joystickChannel = 0;
 
 	RobotDrive robotDrive;	// robot drive system
 	Joystick stick;			// only joystick
+	Gyro driveGyro;
 
 public:
+	float Kp = 0.03;
 	Robot() :
 			robotDrive(frontLeftChannel, rearLeftChannel,
 					   frontRightChannel, rearRightChannel),	// these must be initialized in the same order
-			stick(joystickChannel)								// as they are declared above.
+			stick(joystickChannel), driveGyro(0)								// as they are declared above.
 	{
 		robotDrive.SetExpiration(0.1);
-		robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert the left side motors
+		robotDrive.SetInvertedMotor(RobotDrive::kFrontRightMotor, true);	// invert the left side motors
 		robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);	// you may need to change or remove this to match your robot
 	}
 
@@ -34,11 +35,13 @@ public:
 	void OperatorControl()
 	{
 		robotDrive.SetSafetyEnabled(false);
+		driveGyro.Reset();
+		float angle = driveGyro.GetAngle();
 		while (IsOperatorControl() && IsEnabled())
 		{
         	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
         	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-			robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ());
+			robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ(), -angle *Kp);
 
 			Wait(0.005); // wait 5ms to avoid hogging CPU cycles
 		}
