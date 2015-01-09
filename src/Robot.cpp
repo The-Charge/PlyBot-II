bbd47390c14563b7aa5ad366b39976bb099165aa
Mike Rehberg <Sparky@TR182.com>
2015-01-08 21:38:18 -0500

    Added comments

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -7,26 +7,27 @@ class Robot: public SampleRobot
 {
 
     // Channels for the wheels
-    const static int frontLeftChannel	= 1;
-    const static int rearLeftChannel	= 0;
-    const static int frontRightChannel	= 3;
-    const static int rearRightChannel	= 2;
-    const static int joystickChannel = 0;
+    const static int frontLeftChannel	= 1;		// PWM Port Number
+    const static int rearLeftChannel	= 0;		// PWM Port Number
+    const static int frontRightChannel	= 3;		// PWM Port Number
+    const static int rearRightChannel	= 2;		// PWM Port Number
+    const static int joystickChannel	= 0;		// First and only Joystick
+    const static int gyroChannel		= 0;		// Gyro in Analog Input "0"
 
 	RobotDrive robotDrive;	// robot drive system
 	Joystick stick;			// only joystick
-	Gyro driveGyro;
+	Gyro driveGyro;			// adding one gyro
 
 public:
-	float Kp = 0.03;
+	float Kp = 0.03;		// Proportional constant for the Gyro
 	Robot() :
 			robotDrive(frontLeftChannel, rearLeftChannel,
 					   frontRightChannel, rearRightChannel),	// these must be initialized in the same order
 			stick(joystickChannel), driveGyro(0)								// as they are declared above.
 	{
 		robotDrive.SetExpiration(0.1);
-		robotDrive.SetInvertedMotor(RobotDrive::kFrontRightMotor, true);	// invert the left side motors
-		robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);	// you may need to change or remove this to match your robot
+		robotDrive.SetInvertedMotor(RobotDrive::kFrontRightMotor, true);	// invert the right side motors
+		robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor,   true);	// you may need to change or remove this to match your robot
 	}
 
 	/**
@@ -35,13 +36,13 @@ public:
 	void OperatorControl()
 	{
 		robotDrive.SetSafetyEnabled(false);
-		driveGyro.Reset();
-		float angle = driveGyro.GetAngle();
+		driveGyro.Reset();							// Zero position based on where the robot is pointing now.
+		float angle = driveGyro.GetAngle();			// ***** THIS NEEDS TO BE IN THE WHILE SECTION  ********
 		while (IsOperatorControl() && IsEnabled())
 		{
         	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
         	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-			robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ(), -angle *Kp);
+			robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ(), -angle * Kp);
 
 			Wait(0.005); // wait 5ms to avoid hogging CPU cycles
 		}
